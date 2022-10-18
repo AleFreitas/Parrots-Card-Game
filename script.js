303109c4aa21f07e2d15b2e3081e058148ea38b2
Alexandre <alexandrefsoficial@gmail.com>
2022-10-18 17:24:58 -0300

    reset game option added

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 /*prompt for card number */
 let cardNumber = prompt("insira o numero de cartas a serem utilizadas ( deve ser um par entre 4 e 14)")
 /*number of right moves to win */
-let toWin = cardNumber/2
+let toWin = cardNumber / 2
 /*querySelector for game format based on cardNumber*/
 const gameCards = document.querySelector(".game");
-gameCards.classList.add("cards-"+cardNumber)
+gameCards.classList.add("cards-" + cardNumber)
 /*showing cards based on cardNumber*/
 let cards = []
-for (let i = 1; i <= cardNumber; i++){
+for (let i = 1; i <= cardNumber; i++) {
     let hiddenCard = document.querySelector(".card.hidden");
     hiddenCard.classList.remove("hidden");
 }
 /*implementing card pairs on a array*/
-for (let i = 1; i <= cardNumber/2; i++){
+for (let i = 1; i <= cardNumber / 2; i++) {
     cards.push(i);
     cards.push(i);
 }
@@ -21,16 +21,16 @@ cards.sort(comparador);
 console.log(cards)
 /*[1,3,2,2,1,3] */
 /*list with gifs in order */
-let gifList = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif",
-"metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"]
+let gifList = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif",
+    "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
 console.log(gifList)
 
 /*setting gifs on their respective cards*/
-for (let i = 1; i <= cardNumber; i++){
-    let gifPosition = cards[i-1]
-    let cardGif = gifList[gifPosition-1]
-    let img = document.querySelector(".card"+i+" > .card-back > img")
-    img.setAttribute('src',"media/"+cardGif)
+for (let i = 1; i <= cardNumber; i++) {
+    let gifPosition = cards[i - 1]
+    let cardGif = gifList[gifPosition - 1]
+    let img = document.querySelector(".card" + i + " > .card-back > img")
+    img.setAttribute('src', "media/" + cardGif)
 }
 /*marks witch cards have been selected */
 let selectedCards = []
@@ -39,40 +39,45 @@ let moves = 0
 /*already chosen cards that were right*/
 let alreadyChosen = []
 
-function comparador(){ 
-	return Math.random() - 0.5; 
+function comparador() {
+    return Math.random() - 0.5;
 }
-function toggleCard(card){
-    let cardFront = document.querySelector(".card"+card+" > .card-front");
-    let cardBack = document.querySelector(".card"+card+" > .card-back");
+function toggleCard(card) {
+    let cardFront = document.querySelector(".card" + card + " > .card-front");
+    let cardBack = document.querySelector(".card" + card + " > .card-back");
     cardFront.classList.toggle("hidden");
     cardBack.classList.toggle("hidden");
 }
-function selectCard(card){
-    if (!(alreadyChosen.includes(card))){
+function selectCard(card) {
+    if (!(alreadyChosen.includes(card))) {
         moves++;
         /*showing selected card*/
         toggleCard(card)
         /*choosing the first card */
-        if (selectedCards.length === 0){
-            selectedCards.push([card,cards[card-1]]);
+        if (selectedCards.length === 0) {
+            selectedCards.push([card, cards[card - 1]]);
         /*choosing the second card */
-        }else{
+        } else {
             /*got it right*/
-            if (cards[card-1] === selectedCards[0][1]){
-                toWin-=1;
-                alreadyChosen.push(card)
-                alreadyChosen.push(selectedCards[0][0])
-                selectedCards = []
-                if(toWin === 0){
-                    alert("Você ganhou em "+moves+" jogadas!")
-                }      
+            if (cards[card - 1] === selectedCards[0][1]) {
+                toWin -= 1;
+                alreadyChosen.push(card);
+                alreadyChosen.push(selectedCards[0][0]);
+                selectedCards = [];
+                if (toWin === 0) {
+                    alert("Você ganhou em " + moves + " jogadas!");
+                    reset = prompt("Você gostaria de reiniciar o jogo?");
+                    if (reset === "sim"){
+                        window.location.reload()
+                    }
+
+                }
             /*got it wrong*/
-            }else{
+            } else {
                 console.log("deu ruim")
-                toggleCard(card)
-                toggleCard(selectedCards[0][0])
-                selectedCards = [] 
+                toggleCard(card);
+                toggleCard(selectedCards[0][0]);
+                selectedCards = [];
             }
         }
     }
