bf42a92a2a6eb4c4c3cb79cc80676ded9f595028
Alexandre <alexandrefsoficial@gmail.com>
2022-10-18 15:53:40 -0300

    cards gifs implemented based on card shuffle

--- script.js
+++ script.js
@@ -16,9 +16,22 @@ for (let i = 1; i <= cardNumber/2; i++){
 }
 /*shuffling the cards*/
 cards.sort(comparador);
-
 console.log(cards)
+/*[1,3,2,2,1,3] */
+/*list with gifs in order */
+let gifList = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif",
+"metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"]
+console.log(gifList)
 
-function comparador() { 
+/*setting gifs  */
+for (let i = 1; i <= cardNumber; i++){
+    let gifPosition = cards[i-1]
+    let cardGif = gifList[gifPosition-1]
+    let img = document.querySelector(".card"+i+" > .card-back > img")
+    console.log(cardGif)
+    console.log(img.classList)
+    img.setAttribute('src',"media/"+cardGif)
+}
+function comparador(){ 
 	return Math.random() - 0.5; 
 }
